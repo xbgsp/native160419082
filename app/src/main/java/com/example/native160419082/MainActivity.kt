1a64780b6dac90f112c25755f220930b2693274a
xbgsp <bagusp248@gmail.com>
2021-08-12 14:18:08 +0700

    commit ketiga nambah nama pada toast

--- app/src/main/java/com/example/native160419082/MainActivity.kt
+++ app/src/main/java/com/example/native160419082/MainActivity.kt
@@ -5,10 +5,12 @@ import android.os.Bundle
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private var nama:String = "Bagus"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Toast.makeText(this, "Welcome to Android", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Welcome to Android $nama", Toast.LENGTH_SHORT).show()
     }
 }
